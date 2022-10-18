3f0805ac2174348f29725d8abc9024c9571cb41b
Jaskbro <91650579+Jaskbro@users.noreply.github.com>
2022-10-18 21:59:14 +0900

    오늘 마지막

--- Assets/03.Scripts/Fluid/Combination.cs
+++ Assets/03.Scripts/Fluid/Combination.cs
@@ -20,7 +20,7 @@ public class Combination : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision) // 접촉시 기존 오브젝트들 삭제 후 지정된 오브젝트 생성
     {
-        if (collision.collider.gameObject.CompareTag("MergeTest"))
+        if (collision.collider.gameObject.CompareTag("Object"))
         {
             Debug.Log("접촉");
             Instantiate(OrangeCup, transform.position, Quaternion.identity);
--- Assets/03.Scripts/Fluid/FillLiquid.cs
+++ Assets/03.Scripts/Fluid/FillLiquid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Obi;
 using UnityEngine;
 
 public class FillLiquid : MonoBehaviour
@@ -7,35 +8,48 @@ public class FillLiquid : MonoBehaviour
 
     float a;
     float b;
+    //private void OnParticleCollision(GameObject other)
+    //{
+    //    Debug.Log("¡¢√À«‘");
+    //}
+
+
     void Start()
     {
         Renderer r = GetComponent<Renderer>();
         r.material.shader = Shader.Find("BitshiftProgrammer/Liquid");
-        //a = transform.GetComponent<Renderer>().material.GetFloat("_FillAmount");
+        a = transform.GetComponent<Renderer>().material.GetFloat("_FillAmount");
         transform.GetComponent<Renderer>().material.SetFloat("_FillAmount", 1.6f);
-        Debug.Log("Amount Value is" + a);
+       Debug.Log("Amount Value is" + a);
     }
 
     // Update is called once per frame
     void Update()
     {
-        FillA();
-        a--;
+        //FillA();
+        //a--;
     }
 
-    void FillA()
-    {
+    //void FillA()
+    //{
         
-    }
+    //}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.gameObject.CompareTag("drink"))
-        {
-            Debug.Log("¿Ω∑· ¡¢√À");
-            a=transform.GetComponent<Renderer>().material.GetFloat("_FillAmount");
-            a--;
-        }
-    }
+    //private void OnCollisionEnter(ObiCollider collision)
+    //{
+    //    if (collision.collider.gameObject.CompareTag("drink")) 
+    //    {
+    //        Debug.Log("¿Ω∑· ¡¢√À");
+    //        a=transform.GetComponent<Renderer>().material.GetFloat("_FillAmount");
+    //        a--;
+    //    }
+    //}
+
+    //void OnParticleCollision(GameObject other)
+    //{
+    //    Debug.Log("Particle hit!");
+    //}
+
+    
 
 }
