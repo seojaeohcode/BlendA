1c34d945f20e46cbbbacbc5bd28e1f8d817da733
taehyeon kong <90062866+seojaeohcode@users.noreply.github.com>
2022-10-20 11:29:44 +0900

    텍스트고정

--- Assets/03.Scripts/UI/textPosition.cs
+++ Assets/03.Scripts/UI/textPosition.cs
@@ -4,21 +4,68 @@ using UnityEngine;
 
 public class textPosition : MonoBehaviour
 {
-    public bool isX = false;
-    public bool isZ = false;
+    public GameObject Cam;
 
-    // Start is called before the first frame update
+    Vector3 startScale;
+    public float distance = 3;
+
     void Start()
     {
+        startScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(Cam.transform.position, transform.position);
+        Vector3 newScale = startScale * dist / distance;
+        transform.localScale = newScale;
+
+        transform.rotation = Cam.transform.rotation;
+    }
+    /*
+    public GameObject bottle;
+    public float positionX = 0;
+    public float positionY = 0;
+    public float positionZ = 0;
+    public bool isRotate = true;
+    
+    public GameObject Cam;
+    private void Start()
+    {
+        
+        positionX = bottle.transform.position.x;
+        positionY = bottle.transform.position.y + 1.6f;
+        positionZ = bottle.transform.position.z;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.rotation.eulerAngles.z != 0 || gameObject.transform.rotation.eulerAngles.x !=0)
+        gameObject.transform.rotation = Cam.transform.rotation;
+        
+        if (gameObject.transform.position.x != 0 || gameObject.transform.position.z != 0)
         {
+            transform.position = new Vector3(bottle.transform.position.x, positionY, bottle.transform.position.z);
+        }
 
-        }   
+        if (gameObject.transform.rotation.eulerAngles.z != 0 || gameObject.transform.rotation.eulerAngles.x != 0)
+        {
+            if (isRotate)
+            {
+                positionX = bottle.transform.position.x;
+                positionZ = bottle.transform.position.z;
+                isRotate = false;
+            }
+           
+            transform.position = new Vector3(positionX, positionY, positionZ);
+            transform.rotation = Quaternion.Euler(0, gameObject.transform.rotation.eulerAngles.y, 0);
+        }
+        else
+        {
+            isRotate = true;
+        }
+        
     }
+    */
 }
